5c42e8796779017c44ab7ca64eb1f74a9fc14840
kunghwan <ysw03031@naver.com>
2024-12-12 12:38:53 +0900

    init

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="">질문이 있으신가요? 문의 전화 : <span>00-308-321-0161(수신자부담)</span> </a>
+      <br />
+      <br />
+      <ul>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+        <li><button>자주 묻는 질문</button></li>
+      </ul>
+      <br />
+      <br />
+      <button className='bt1'>한국어</button>
+      <br />
+      <br />
+      <p>sdfsdfsdf</p>
+      <br />
+      <br />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eos voluptates saepe. Nisi natus fuga accusantium consequatur sint quibusdam labore reiciendis fugit? Doloremque, sapiente a deleniti dignissimos harum commodi in?</p>
+    </footer>
+  )
+}
+
+export default Footer
